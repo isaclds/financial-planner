a7279ff0e1129cc1a58d8154f939d8507b7623a5
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-09-12 15:16:27 -0300

    feat: expense categories routes config

--- api-finance-tracker/src/routes/expenses.route.ts
+++ api-finance-tracker/src/routes/expenses.route.ts
@@ -1,17 +1,24 @@
 import { Router } from 'express';
 import { ExpenseCategoryController } from '../controllers/index';
+import { HttpStatus } from '@src/config/status';
 
 const router = Router();
 const categoryController = new ExpenseCategoryController();
 
-router.get('/', (req, res) => {
-  res.json({
-    _id: 'ABC123',
-    name: 'Product Name',
-    price: 28.9,
-  });
+router.use((_req, res) => {
+  res.status(HttpStatus.NOT_FOUND).json({ error: 'Route not found' });
 });
 
 router.get('/categories', (req, res) => categoryController.findAll(req, res));
+router.get('/categories/:id', (req, res) =>
+  categoryController.findById(req, res),
+);
+router.post('/categories', (req, res) => categoryController.create(req, res));
+router.put('/categories/:id', (req, res) =>
+  categoryController.update(req, res),
+);
+router.delete('/categories/:id', (req, res) =>
+  categoryController.delete(req, res),
+);
 
 export default router;
